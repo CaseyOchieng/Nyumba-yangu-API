189cbde5cae902151b73a58435e04ec8eb270cbf
Casey Ochieng <caseyochieng46@gmail.com>
2024-11-20 16:26:43 +0300

    .env.example

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 
+dotenv.config();
+
 const port = 3000;
+const Mongoclient = process.env.MONGODB_URI;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost:27017/mern-estate");
 app.listen(port, () => {
+  mongoose.connect(Mongoclient).then(() => {
+    console.log("Connected to MongoDB");
+  });
   console.log(`Server is running on http://localhost:${port}`);
 });
