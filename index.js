90fcaf84d131bf81bfeebca540910d42ce867b3e
Casey Ochieng <caseyochieng46@gmail.com>
2024-11-20 16:30:00 +0300

    .env.example

--- index.js
+++ index.js
@@ -10,8 +10,13 @@ const Mongoclient = process.env.MONGODB_URI;
 const app = express();
 
 app.listen(port, () => {
-  mongoose.connect(Mongoclient).then(() => {
-    console.log("Connected to MongoDB");
-  });
+  mongoose
+    .connect(Mongoclient)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
+    });
   console.log(`Server is running on http://localhost:${port}`);
 });
